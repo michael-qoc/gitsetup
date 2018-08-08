655860646dbb9f5066bc46dafa9c087e0f208e7b
Michael Li <michael.li@QoCs-MacBook-Pro-2.local>
2018-08-07 23:12:32 -0400

    git connection with working laptop setup

---
+++ test.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>www</h1>
+        div div
+      </div>
+    );
+  }
+}
